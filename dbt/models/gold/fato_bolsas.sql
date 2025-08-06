693ebba331e75e0d40bef630ad0b65eb0b4f3032
MarcellOliveiraDux <marcell.oliveira@duxnutrition.com>
2025-08-06 15:44:56 -0300

    remocao do bolsista

--- dbt/models/gold/fato_bolsas.sql
+++ dbt/models/gold/fato_bolsas.sql
@@ -2,7 +2,6 @@ SELECT
     CAST(_id AS TEXT) AS id,
     CAST(ano AS INTEGER) AS ano,
     CAST(idbolsa AS TEXT) AS id_bolsa,
-    CAST(bolsista AS TEXT) AS bolsista,
     CAST(valortotalprevisto AS FLOAT) AS valortotalprevisto,
     CAST(datadisponibilizacao AS DATE) AS data_disponibilizacao
 FROM {{ ref('bolsas-de-cotas-concedidas') }}
